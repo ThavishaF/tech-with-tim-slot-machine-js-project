e4e9fa09ce4e79d1a37e4a7afafb7c40d595b7e8
Thavisha Fernando <thavishafdo@gmail.com>
2025-05-17 21:13:40 +0530

    Commit the new files to start the dev process

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Building a slot machine 
+
+Create a new folder 
+Create a new file inside that folder named "project.js" 
+
+Open a terminal in side the folder and run the folowing comands:
+    npm init
+    npm i prompt-sync
+
---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. deposit some money 
+// 2. Determine the number of lines to bet on 
+// 3. collect the bet amount 
+// 4. Spin the slot machine 
+// 5. Check if the user won
+// 6. Give the user their winnings 
+// 7. play again
